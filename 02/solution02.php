9148dc7a2c0848aac064cd144b5853e590861a9c
Oleg Gorbunov <oleg.gorbunov@gmail.com>
2021-12-03 16:44:02 +0600

    Day 02/2 solution

--- 02/solution02.php
+++ 02/solution02.php
@@ -4,6 +4,7 @@ require_once __DIR__.'/../vendor/autoload.php';
 use function Gorbunov\functional\suppport\dd;
 use function Gorbunov\functional\collection\pair;
 use function Gorbunov\functional\composition\pipe;
+use function Gorbunov\functional\collection\slice;
 use function Gorbunov\functional\collection\mapper;
 use function Gorbunov\functional\collection\reducer;
 use function Gorbunov\functional\composition\compose;
@@ -23,12 +24,31 @@ $commands = pipe(
 );
 
 $solver01 = compose(
-    reducer([0, 0], fn($carry, $command) => match ($command[0]) {
-        CommandList::FORWARD => [$carry[0] + $command[1], $carry[1]],
-        CommandList::DOWN => [$carry[0], $carry[1] + $command[1]],
-        CommandList::UP => [$carry[0], max($carry[1] - $command[1], 0)],
-    }),
+    reducer([0, 0, 0],
+        fn($carry, $command) => match ($command[0]) {
+            CommandList::FORWARD => [$carry[0] + $command[1], $carry[1]],
+            CommandList::DOWN => [$carry[0], $carry[1] + $command[1]],
+            CommandList::UP => [$carry[0], max($carry[1] - $command[1], 0)],
+        }
+    ),
+    array_product(...)
+);
+
+$solver02 = compose(
+    reducer(
+        [0, 0, 0],
+        fn($carry, $command) => match ($command[0]) {
+            CommandList::FORWARD => [$carry[0] + $command[1], $carry[1] + $command[1] * $carry[2], $carry[2]],
+            CommandList::DOWN => [$carry[0], $carry[1], $carry[2] + $command[1]],
+            CommandList::UP => [$carry[0], $carry[1], $carry[2] - $command[1]],
+        },
+    ),
+    slice(0, 2),
+    array_product(...),
 );
 
 $position = $solver01($commands);
-printf("---\nNew position : %d\n", array_product($position));
+$aim = $solver02($commands);
+
+printf("---\nNew position : %d\n", $position);
+printf("---\nNew aim : %d\n", $aim);
